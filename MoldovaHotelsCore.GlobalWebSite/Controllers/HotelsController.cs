c7122e4341fa5c6bf52d4ae80826afff7efd1170
Alexei Pancratov <drivergear@gmail.com>
2018-12-09 23:55:05 +0200

    Improved exception handlig in HotelsController

--- MoldovaHotelsCore.GlobalWebSite/Controllers/HotelsController.cs
+++ MoldovaHotelsCore.GlobalWebSite/Controllers/HotelsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MoldovaHotelsCore.GlobalWebSite.Models;
 using MoldovaHotelsCore.GlobalWebSite.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -17,16 +18,32 @@ namespace MoldovaHotelsCore.GlobalWebSite.Controllers
 
         public async Task<IActionResult> Index()
         {
-            IEnumerable<Hotel> hotels = await hotelService.GetHotelsAsync();
+            IEnumerable<Hotel> hotels;
+
+            try
+            {
+                hotels = await hotelService.GetHotelsAsync();
+            }
+            catch (Exception)
+            {
+                hotels = new List<Hotel>();
+            }
 
             return View(hotels);
         }
 
         public async Task<IActionResult> Show(int hotelId)
         {
-            Hotel hotel = await hotelService.GetHotelAsync(hotelId);
+            try
+            {
+                Hotel hotel = await hotelService.GetHotelAsync(hotelId);
 
-            return View(hotel);
+                return View(hotel);
+            }
+            catch (Exception)
+            {
+                return View("Error", new ErrorViewModel());
+            }
         }
     }
 }
--- MoldovaHotelsCore.GlobalWebSite/Startup.cs
+++ MoldovaHotelsCore.GlobalWebSite/Startup.cs
@@ -57,7 +57,7 @@ namespace MoldovaHotelsCore.GlobalWebSite
             return HttpPolicyExtensions
                 .HandleTransientHttpError()
                 .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
-                .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+                .WaitAndRetryAsync(2, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
