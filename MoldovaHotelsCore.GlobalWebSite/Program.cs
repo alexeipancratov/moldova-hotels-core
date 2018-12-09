db66b6ce5cbc208fd56c9b9736ddfaf444542741
Alexei Pancratov <drivergear@gmail.com>
2018-12-09 11:02:28 +0200

    Made microservices URLs configurable

--- MoldovaHotelsCore.GlobalWebSite/Program.cs
+++ MoldovaHotelsCore.GlobalWebSite/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -19,6 +14,11 @@ namespace MoldovaHotelsCore.GlobalWebSite
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                })
                 .UseStartup<Startup>();
     }
 }
